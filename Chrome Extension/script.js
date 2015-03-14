6c4a7cbecf770dc42515e98549ce5427168fb17d
Pavlo Kyrychenko <web93onv@gmail.com>
2015-03-14 13:19:31 +0200

    Загрузка файлов

---
+++ Chrome Extension/script.js
@@ -0,0 +1,16 @@
+﻿function init() {
+  var script = document.createElement('script');
+  script.innerHTML = "window.player_ad_url_1='';window.player_ad_url_2='';window.player_ad_url_3='';console.log('EX.ua без рекламы!');";
+  var head = (document.head || document.body);
+  head.appendChild(script);
+}
+
+if(/loaded|complete|interactive/.test(document.readyState) || document.loaded) {
+	init();
+} else {
+  if(window.addEventListener) {
+    window.addEventListener('DOMContentLoaded', init, false);
+  } else {
+    window.attachEvent('onload', init);
+  }
+}
